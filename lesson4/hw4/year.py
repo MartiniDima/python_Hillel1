63520f9889e1ab598f2287b5ae1bbc6093750cc1
MartiniDima <martinidima@gmail.com>
2022-10-26 14:26:29 +0300

    сделал календарь для определения високостный год или нет

--- lesson4/hw4/year.py
+++ lesson4/hw4/year.py
@@ -2,14 +2,15 @@ year = int(input("Введите год для проверки:"))
 
 if year >= 1900 and year <= 1000000:
     print("Спасибо ")
+    y = year
+    if y % 4 == 0 and y % 400 == 0 and y % 100 == 0:
+        print(y, "высокостный год")
+    else:
+        print(y, " НЕ высокостный год")
 else:
     print("неверные параметры ввода")
 
-if year % 4 == 0 and year % 400 == 0 and year % 100 == 0:
-    print(year, "высокостный год")
-else:
-    print(year, " НЕ высокостный год")
 
 
 
-#
+
