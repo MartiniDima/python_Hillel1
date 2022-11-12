7d08ac3edd45591b1424561b360110e14ffd1448
MartiniDima <martinidima@gmail.com>
2022-11-12 20:04:43 +0300

    сделал кредитный калькулятор , немного переделывал

--- lesson7/hw1/kredit_calc.py
+++ lesson7/hw1/kredit_calc.py
@@ -12,28 +12,28 @@ opl_mes = 0
 sum_proc = 0
 while True:
 
-        if opl_mes > 24 and opl_mes < o_period:
+    if opl_mes <= 23 and opl_mes < o_period :
+        ostat_period_mes -= 1
+        opl_mes += 1
+        plat_bez_procenta = ostat_summa / o_period
+        ostat_summa -= plat_bez_procenta
+        procent = plat_bez_procenta / 100 * 2
+        plata_s_procentom = plat_bez_procenta + procent
+        sum_proc += procent
+        print("месяц выплаты   %.f " % (opl_mes), "сумма %.2f" % (plata_s_procentom),
+              f" {o_procent:>10}% в месяц ")
+
+    elif opl_mes >= 24 and opl_mes < o_period:
+
             opl_mes += 1
             plat_bez_procenta = ostat_summa / o_period
             ostat_summa -= plat_bez_procenta
             procent = plat_bez_procenta / 100 * o_procent2
             plata_s_procentom = plat_bez_procenta + procent
             sum_proc += procent
-            print("месяц выплаты 4%", opl_mes,"сумма",  plata_s_procentom)
+            print("месяц выплаты   %.f " % (opl_mes), "сумма %.2f" % (plata_s_procentom),
+                  f" {o_procent2:>10}% в месяц "
 
-        if opl_mes <= 24 and opl_mes < o_period:
-            ostat_period_mes -= 1
-            opl_mes += 1
-            plat_bez_procenta = ostat_summa/o_period
-            ostat_summa -= plat_bez_procenta
-            procent = plat_bez_procenta/100*2
-            plata_s_procentom = plat_bez_procenta + procent
-            sum_proc += procent
-            print("месяц выплаты 2%", opl_mes,"сумма",  plata_s_procentom)
-
-
-
-
-
+                  )
 
 
