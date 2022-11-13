671fa2fbab01ebd31f55f2193d2bb6ce6ea8ca34
MartiniDima <martinidima@gmail.com>
2022-11-13 11:46:54 +0300

    выполнено

---
+++ lesson 8/hw2/split.py
@@ -0,0 +1,16 @@
+str = str(input("введите предложение:"))
+
+s = list(str.split())
+
+new_list = sorted(s)
+l = len(new_list)
+
+if l <= 2:
+    str = str(input("введите больше двух слов:"))
+else:
+    print(s)
+    print(new_list)
+    print("количество слов в списке", l)
+    for i in new_list:
+        x = new_list.index(i)
+        print( '{:<15}'.format(x), "{:>15}".format(i))
