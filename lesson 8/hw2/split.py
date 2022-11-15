d961f4313e6e07ddc2c81ebfcc6fdd3677287c5c
MartiniDima <martinidima@gmail.com>
2022-11-15 11:17:50 +0300

    отсортировал введенную строку и вывел индексы отсортированных строк

--- lesson 8/hw2/split.py
+++ lesson 8/hw2/split.py
@@ -1,12 +1,12 @@
 str = str(input("введите предложение:"))
 
 s = list(str.split())
-
+stroka_new = s
 new_list = sorted(s)
 l = len(new_list)
 
 if l <= 2:
-    str = str(input("введите больше двух слов:"))
+    stroka_new = str(input("введите больше двух слов:"))
 else:
     print(s)
     print(new_list)
