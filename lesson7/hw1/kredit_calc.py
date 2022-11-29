ce0ce6bb9ab638ea0c5b0c5433875ebdd630c881
MartiniDima <martinidima@gmail.com>
2022-11-29 16:15:54 +0300

    понимаю что нужно было постараться сделать все в одну строку но пока сложно. А времени не хватает свет дают очень тяжко + на службе постараюсь доганять программу

--- lesson7/hw1/kredit_calc.py
+++ lesson7/hw1/kredit_calc.py
@@ -1,15 +1,16 @@
 sum = int(input("введите сумму кредита:"))
 per = int(input("введите количество лет:"))
-o_summa = sum
+o_summa = sum/100*80
 o_period = per * 12
-o_procent = 2
-o_procent2 = 4
-
-ostat_summa = sum
+o_procent = 0.25
+o_procent2 = 0.25
+print(o_summa)
+ostat_summa = o_summa
 ostat_period_mes = int(o_period)
-
+pr = 0
 opl_mes = 0
 sum_proc = 0
+
 while True:
 
     if opl_mes <= 23 and opl_mes < o_period :
@@ -18,6 +19,7 @@ while True:
         plat_bez_procenta = ostat_summa / o_period
         ostat_summa -= plat_bez_procenta
         procent = plat_bez_procenta / 100 * 2
+        pr += procent
         plata_s_procentom = plat_bez_procenta + procent
         sum_proc += procent
         print("месяц выплаты   %.f " % (opl_mes), "сумма %.2f" % (plata_s_procentom),
@@ -29,6 +31,7 @@ while True:
             plat_bez_procenta = ostat_summa / o_period
             ostat_summa -= plat_bez_procenta
             procent = plat_bez_procenta / 100 * o_procent2
+            pr += procent
             plata_s_procentom = plat_bez_procenta + procent
             sum_proc += procent
             print("месяц выплаты   %.f " % (opl_mes), "сумма %.2f" % (plata_s_procentom),
