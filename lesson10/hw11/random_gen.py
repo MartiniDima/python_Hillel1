7a10ee7826aa61c64b20ed5c134954e49e91f0aa
MartiniDima <martinidima@gmail.com>
2022-12-28 14:53:04 +0300

    переделал в одну строку

--- lesson10/hw11/random_gen.py
+++ lesson10/hw11/random_gen.py
@@ -1,9 +1,3 @@
 import random
-list = [random.randint(1,20) for i in range(20)]
-print('рандомный список:', list)
-set = set(list)
-print("все разные символы:", set)
-cont = 0
-for i in set:
-    cont += 1
-print("разних чисел встречается :",cont)
+
+print(len(set([random.randint(1,20) for i in range(20)])))
